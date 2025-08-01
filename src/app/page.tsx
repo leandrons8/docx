73db1fc4fc4fdafb7cf02586329d1a7d7f9737ec
Leandro Santos <leandrons8@gmail.com>
2025-08-01 10:42:35 -0300

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,15 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import { patch } from "./teste";
 
 export default function Home() {
+  function DownloadTemplate(){
+    let prefix
+    if (process.env.PAGES_BASE_PATH){
+      prefix = process.env.PAGES_BASE_PATH
+    } else {
+      prefix = ""
+    }
+    return <a href={`${prefix}/model.docx`} download>Download Document</a>
+  }
   return (
     <Container>
       <Row>
